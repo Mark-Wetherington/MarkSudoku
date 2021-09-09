d52f736aca242b2dc5a5bfeb1f1fce5061b7f935
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-09-08 21:25:20 -0400

    started modal - wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Title from "./Components/Title";
 import Game from "./Components/Game/Game";
+import Modal from "./Components/UI/Modal";
 
 function App() {
   return (
     <>
       <Title />
       <Game />
+      <Modal />
     </>
   );
 }
---
+++ src/Components/UI/Modal.js
@@ -0,0 +1,22 @@
+import ReactDOM from "react-dom";
+
+import Card from "../UI/Card";
+import styles from "./Modal.module.css";
+
+const Popup = (props) => {
+  return <Card className={styles.modal}>aaa</Card>;
+};
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <div className={styles.backdrop}></div>,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(<Popup />, document.getElementById("popup-root"))}
+    </>
+  );
+};
+
+export default Modal;
