360cb4bf5a6dfde4f987b75786d9e8b842715db9
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-10-10 14:02:12 -0400

    color updates

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import Title from "./Components/Title";
 import Game from "./Components/Game/Game";
 import { ModalProvider } from "./Components/UI/Modal";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <>
+    <div className={styles.app}>
       <Title />
       <ModalProvider>
         <Game />
       </ModalProvider>
-    </>
+    </div>
   );
 }
 
