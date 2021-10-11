8203ed75c40ac78570208ee671553384e73c3786
Mark Wetherington <mark.wetherington@ecksbee.com>
2021-10-11 11:04:37 -0400

    refactored Cell.js to allow backspace and delete

--- src/Components/Game/Cell.js
+++ src/Components/Game/Cell.js
@@ -1,10 +1,10 @@
 import styles from "./Cell.module.css";
 
-const ALLOWED_KEYS = /[1-9]/;
+const ALLOWED_KEYS = ["Delete", "Backspace"];
 
 const Cell = (props) => {
   const validateInput = (event) => {
-    if (!ALLOWED_KEYS.test(event.key)) {
+    if (isNaN(Number(event.key)) && !ALLOWED_KEYS.includes(event.key)) {
       event.preventDefault();
     }
   };
@@ -16,10 +16,11 @@ const Cell = (props) => {
 
   return (
     <input
-      type="text"
+      type="number"
       name="sudoku-cell"
-      pattern="[1-9]"
-      maxLength="1"
+      min="1"
+      max="9"
+      step="1"
       defaultValue={props.value}
       readOnly={props.isGiven}
       className={classList.map((thisClass) => styles[`${thisClass}`]).join(" ")}
@@ -27,6 +28,9 @@ const Cell = (props) => {
       onKeyDown={validateInput}
       onPaste={(e) => e.preventDefault()}
       onChange={(e) => {
+        if (e.target.value.length > 1) {
+          e.target.value = e.target.value % 10;
+        }
         props.onChange(props.idx, Number(e.target.value));
       }}
     />
